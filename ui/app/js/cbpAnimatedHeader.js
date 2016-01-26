7fa19f279fcbbc5209edd8cb9ed707f6794dd7b7
butjaarl13 <arlinda.butja@edu.fh-joanneum.at>
2016-01-26 17:39:27 +0100

    Layout mit ng-view angepasst

--- ui/app/js/cbpAnimatedHeader.js
+++ ui/app/js/cbpAnimatedHeader.js
@@ -4,7 +4,7 @@
  *
  * Licensed under the MIT license.
  * http://www.opensource.org/licenses/mit-license.php
- * 
+ *
  * Copyright 2013, Codrops
  * http://www.codrops.com
  */
@@ -13,7 +13,7 @@ var cbpAnimatedHeader = (function() {
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-default' ),
 		didScroll = false,
-		changeHeaderOn = 300;
+		changeHeaderOn = 7;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
@@ -27,10 +27,10 @@ var cbpAnimatedHeader = (function() {
 	function scrollPage() {
 		var sy = scrollY();
 		if ( sy >= changeHeaderOn ) {
-			classie.add( header, 'navbar-shrink' );
+			classie.add( header, 'affix' );
 		}
 		else {
-			classie.remove( header, 'navbar-shrink' );
+			classie.remove( header, 'affix' );
 		}
 		didScroll = false;
 	}
@@ -40,5 +40,4 @@ var cbpAnimatedHeader = (function() {
 	}
 
 	init();
-
 })();
